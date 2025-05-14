8c6d82ad53dc756efbb44507620d009852521412
HyeongjunCH <chj073675@gmail.com>
2025-05-15 00:19:44 +0900

    카드 분배 로직 추가(정답 카드는 봉투에)

--- Clue/Clue/GameState.cs
+++ Clue/Clue/GameState.cs
@@ -15,7 +15,7 @@ namespace Clue
         public int id;
         //public PictureBox player;
 
-        public List<Card> cards;
+        public List<Card> hands; //가지고 있는 카드
 
         public int x; //열
         public int y; //행
@@ -37,7 +37,7 @@ namespace Clue
             this.name = name;
         }
 
-       /* public string Type
+        public string Type
         {
             get { return type; }
             set { type = value; }
@@ -47,7 +47,7 @@ namespace Clue
         {
             get { return name; }
             set { name = value; }
-        }*/
+        }
 
     }
 
@@ -62,7 +62,7 @@ namespace Clue
                 { 1, 1, 1, 4, 1, 1, 0, 0, 1, 1, 1, 4, 1, 1, 1, 1, 0, 0, 1, 1, 1, 4, 1, 1},
                 { 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1},
                 { 1, 1, 1, 1, 1, 1, 0, 0, 2, 1, 1, 1, 1, 1, 1, 2, 0, 0, 0, 2, 1, 1, 1, 1},
-                { 1, 1, 1, 1, 2, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+                { 1, 1, 1, 1, 2, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1},
                 { 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 1, 1, 1, 1, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0},
                 { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1},
                 { 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 1, 1, 1, 1, 1},
@@ -86,7 +86,11 @@ namespace Clue
 
         public int TotalPlayers { get; set; }
         public Player[] Players { get; set; }
-        public List<Card> AllCards { get; set; } = new List<Card>();
+
+        public Card[] answer = new Card[3]; //정답 카드
+        public List<Card> Deck = new List<Card>(); //덱
+        public List<Card> openCard = new List<Card>(); //나눠주고 남아서 공개할 카드들
+
         public int CurrentTurn { get; set; } = 0;
         public Point[,] clue_map_point { get; set; }
 
@@ -107,6 +111,61 @@ namespace Clue
         public static int[,] GetInitialMap()
         {
             return new int[25, 24];
+        }
+
+        public void InitializeCards()
+        {
+            string[] types = { "mans", "weapon", "room" };
+            string[] man = { "Green", "Mustard", "Peacock", "Plum", "Scarlett", "White" };
+            string[] weapon = { "촛대", "파이프", "리볼버", "밧줄", "렌치", "단검" };
+            string[] room = { "주방", "공부방", "무도회장", "온실", "식당", "당구장", "서재", "라운지", "홀" };
+
+            //정답 카드 생성, 봉투에 넣기
+            Random rand = new Random();
+            answer[0] = new Card(types[0], man[rand.Next(0, man.Length + 1)]);
+            answer[1] = new Card(types[1], weapon[rand.Next(0, weapon.Length + 1)]);
+            answer[2] = new Card(types[2], room[rand.Next(0, room.Length + 1)]);
+
+            //나머지 카드를 덱에 삽입
+            for (int i = 0; i < types.Length; i++)
+            {
+                foreach (string temp in types)
+                {
+                    if (temp != answer[i].Name)
+                        Deck.Add(new Card(types[i], temp));
+                }
+            }
+
+            //카드 섞기
+            Random random = new Random();
+            Deck = Deck.OrderBy(x => random.Next()).ToList();
+        }
+
+        public void distributeCards() //카드 분배
+        {
+            int playerCount = Players.Length;
+            int cardCount = Deck.Count;
+            int cardsPerPlayer = cardCount / playerCount; //나눠줘야 할 카드 수
+
+            for(int i = 0; i < playerCount; i++)
+            {
+                Players[i].hands = new List<Card>();
+                for (int j = 0; j < cardsPerPlayer; j++)
+                {
+                    if (Deck.Count > 0)
+                    {
+                        Players[i].hands.Add(Deck[0]);
+                        Deck.RemoveAt(0);
+                    }
+                }
+            }
+
+            //남은 카드를 공개 카드에 추가
+            while (Deck.Count > 0)
+            {
+                openCard.Add(Deck[0]);
+                Deck.RemoveAt(0);
+            }
         }
 
     }
